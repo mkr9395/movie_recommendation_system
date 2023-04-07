3bcb13e6cd24a88456071d13790d8864a95aae49
Mohit Kumar <mkr9395@gmail.com>
2023-04-07 14:34:05 +0530

    meassage needs to be passed

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def recommend(movie):
     movies_list = sorted(list(enumerate(distances)),reverse=True,key = lambda x:x[1])[1:6]
     
     recommended_movies = []
-    
+
     for i in movies_list:
         recommended_movies.append(movies.iloc[i[0]].title)
     return recommended_movies
